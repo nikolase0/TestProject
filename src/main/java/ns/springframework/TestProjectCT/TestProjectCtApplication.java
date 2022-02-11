35d25affee9a7b3ac61dfc4e2ef87f68b6c43da3
nikolase0 <serafimovic28@gmail.com>
2022-02-11 12:01:21 +0100

    Initial commit

---
+++ src/main/java/ns/springframework/TestProjectCT/TestProjectCtApplication.java
@@ -0,0 +1,13 @@
+package ns.springframework.TestProjectCT;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestProjectCtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestProjectCtApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ns/springframework/TestProjectCT/TestProjectCtApplicationTests.java
@@ -0,0 +1,13 @@
+package ns.springframework.TestProjectCT;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestProjectCtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
